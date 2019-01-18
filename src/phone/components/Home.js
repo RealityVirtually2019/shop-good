6c007f8b4634472fd8299e13232ca8e7e33220b9
Tejas Shroff <tejasshroff24@gmail.com>
2019-01-18 13:54:18 -0500

    First commit!

---
+++ src/phone/components/Home.js
@@ -0,0 +1,15 @@
+// $scope, $element, $attrs, $injector, $sce, $timeout, $http, $ionicPopup, and $ionicPopover services are available
+
+
+//$scope.view.wdg["3DLabel-1"]["visible"]=true
+
+//$scope.view.wdg["3DLabel-1"]["text"]="food is good"
+
+// function   example  ...
+
+//$scope.myfunction =function() {
+  
+//$scope.view.wdg["3DLabel-1"]["visible"]=true
+
+//$scope.view.wdg["3DLabel-1"]["text"]="food is good"
+//}
